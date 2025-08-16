69cd6e51bdf0def2755d504c37d8d33db76d94f7
Prince Kumar <pk7411956419@gmail.com>
2025-08-16 14:39:25 +0530

    Completed the primary backend schema and basic code to put data entry in RUn and outbox table

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -1,13 +1,38 @@
-const express = require('express');
+import express from "express";
+import {PrismaClient} from "@prisma/client"
+import { log } from "console";
+
+const client = new PrismaClient();
+
 const app = express();
-const port = 3000;
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
+const PORT = 3000;
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+app.post("/hooks/catch/:userId/:zapId",async (req,res)=>{
+    const userId = req.params.userId;
+    const zapId=req.params.zapId;
+    const body=req.body;
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-});
+    await client.$transaction(async (tx)=>{
+        const run= await tx.zapRun.create({
+            data:{
+                zapId:zapId,
+                metadata:body
+            }
+        })
+
+        await tx.zapRunOutbox.create({
+            data:{
+                // zapRunId:zapId
+                zapRunId:run.id
+            }
+        })
+    })
+    return res.json({
+        msg:"Webhook Received"
+    })
+})
+
+app.listen(PORT,()=>{
+    log("Server running!");
+})
